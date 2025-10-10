68077e8514cd447e0b49f8aded1b9c7288aee089
k.kosterin <kosterin-kirill@mail.ru>
2025-10-10 19:10:31 +0300

    Refactor combined gaming and non-interactive mode

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,41 +1,53 @@
 package dev.kiryao
 
+import dev.kiryao.core.cliui.ConsoleGameView
+import dev.kiryao.core.data.HangmanDb
+import dev.kiryao.feature.playzone.PlayzoneLogic
+import dev.kiryao.feature.playzone.PlayzoneScreen
 import kotlin.system.exitProcess
 
 const val HIDDEN_SYMBOL = '*'
 
 fun main(args: Array<String>) {
-    if (args.size != 2) {
-        println("Unknown word")
-        exitProcess(1)
-    }
 
-    val (targetWord, userInput) = args
+    if (args.size == 2) {
 
-    val normalizedTarget = targetWord.trim().lowercase()
-    val normalizedInput = userInput.trim().lowercase()
+        val (targetWord, userInput) = args
 
-    if (normalizedTarget == normalizedInput) {
-        println("$normalizedInput;POS")
-        exitProcess(0)
-    }
+        val normalizedTarget = targetWord.trim().lowercase()
+        val normalizedInput = userInput.trim().lowercase()
 
-    val result = CharArray(normalizedTarget.length) { HIDDEN_SYMBOL }
+        if (normalizedTarget == normalizedInput) {
+            println("$normalizedInput;POS")
+            exitProcess(0)
+        }
 
-    for (char in normalizedInput) {
-        if (char in normalizedTarget) {
-            for (i in normalizedTarget.indices) {
-                if(normalizedTarget[i] == char && result[i] == HIDDEN_SYMBOL) {
-                    result.openChar(char = char, index = i)
+        val result = CharArray(normalizedTarget.length) { HIDDEN_SYMBOL }
+
+        for (char in normalizedInput) {
+            if (char in normalizedTarget) {
+                for (i in normalizedTarget.indices) {
+                    if (normalizedTarget[i] == char && result[i] == HIDDEN_SYMBOL) {
+                        result.openChar(char = char, index = i)
+                    }
                 }
             }
         }
-    }
 
-    val output = String(result)
-    val messageResult = if (output == normalizedTarget) "POS" else "NEG"
-    println("$output;$messageResult")
-    exitProcess(0)
+        val output = String(result)
+        val messageResult = if (output == normalizedTarget) "POS" else "NEG"
+        println("$output;$messageResult")
+        exitProcess(0)
+    } else {
+
+        val game = PlayzoneScreen(
+            hangmanDb = HangmanDb(),
+            playzoneLogic = PlayzoneLogic(),
+            consoleGameView = ConsoleGameView()
+        )
+
+        game.loop()
+    }
 }
 
 private fun CharArray.openChar(char: Char, index: Int) {
