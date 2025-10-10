9109bf739ac0f85414a9a1ada6c2a527916edaa9
k.kosterin <kosterin-kirill@mail.ru>
2025-10-10 19:12:33 +0300

    Add a file for a non-interactive mod

---
+++ src/main/kotlin/feature/nonInteractive/NonInteractiveMode.kt
@@ -0,0 +1,38 @@
+package dev.kiryao.feature.nonInteractive
+
+import kotlin.system.exitProcess
+
+object NonInteractiveMode {
+    private const val HIDDEN_SYMBOL = '*'
+
+    fun processNonInteractiveMode(targetWord: String, userInput: String) {
+        val normalizedTarget = targetWord.trim().lowercase()
+        val normalizedInput = userInput.trim().lowercase()
+
+        if (normalizedTarget == normalizedInput) {
+            println("$normalizedInput;POS")
+            exitProcess(0)
+        }
+
+        val result = CharArray(normalizedTarget.length) { HIDDEN_SYMBOL }
+
+        for (char in normalizedInput) {
+            if (char in normalizedTarget) {
+                for (i in normalizedTarget.indices) {
+                    if (normalizedTarget[i] == char && result[i] == HIDDEN_SYMBOL) {
+                        result.openChar(char = char, index = i)
+                    }
+                }
+            }
+        }
+
+        val output = String(result)
+        val messageResult = if (output == normalizedTarget) "POS" else "NEG"
+        println("$output;$messageResult")
+        exitProcess(0)
+    }
+
+    private fun CharArray.openChar(char: Char, index: Int) {
+        this[index] = char
+    }
+}
